b44cd7a3d97eb9e97e31e959585e70fddd6e6c2c
Bruno <bcoronado.morales@gmail.com>
2020-10-06 17:31:52 -0600

    Creación del componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,10 +12,12 @@ import { AngularFirestore } from '@angular/fire/firestore';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RegistroComponent } from './autenticacion/registro/registro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule,
