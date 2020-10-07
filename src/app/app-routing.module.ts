94df8a13926836f29ec0e2a0ed2587ce2602c553
Kevin Garcia <kevinalexandergarcia1999@gmail.com>
2020-10-07 12:17:27 -0600

    Se importaron librerías y se creó ruta

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import{PerfilPersonalComponent} from './perfil-personal/perfil-personal.component'
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "perfil-personal",
+    component:PerfilPersonalComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+  
+ }
