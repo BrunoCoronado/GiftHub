95c3a68f9378cc84bf1c3072253ded1e4faa383b
Alan <alanmorataya19@gmail.com>
2020-10-06 20:53:33 -0600

    Se creó el componente para ver el catalogo

---
+++ src/app/ver-catalogo/ver-catalogo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ver-catalogo',
+  templateUrl: './ver-catalogo.component.html',
+  styleUrls: ['./ver-catalogo.component.css']
+})
+export class VerCatalogoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
