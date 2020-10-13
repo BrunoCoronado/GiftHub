21abf52ab03750539fafeb062eefd01fbfe50ed0
Alan <alanmorataya19@gmail.com>
2020-10-13 02:26:15 -0600

    Se lograron obtener las transacciones

--- src/app/detalle-transacciones/detalle-transacciones.component.ts
+++ src/app/detalle-transacciones/detalle-transacciones.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {TransaccionesserviceService} from './transaccionesservice.service'
+import { TransaccionesserviceService } from './transaccionesservice.service'
 @Component({
   selector: 'app-detalle-transacciones',
   templateUrl: './detalle-transacciones.component.html',
@@ -7,40 +7,66 @@ import {TransaccionesserviceService} from './transaccionesservice.service'
 })
 export class DetalleTransaccionesComponent implements OnInit {
   clientesService: TransaccionesserviceService;
-  constructor(clientesService:TransaccionesserviceService) {
+  constructor(clientesService: TransaccionesserviceService) {
     this.clientesService = clientesService;
   }
   public transacciones = [];
+  public array2 = [];
   ngOnInit(): void {
-  
-console.log("Las Transacciones SON : \n")
-console.log(this.transacciones)
+
+    //console.log("Las Transacciones SON : \n")
+    this.casteoTransaccion();
+    this.clientesService.getusuarios2();
+    //console.log(this.transacciones)
   }
 
   public getTransacciones() {
     let transacciones = [];
 
-    transacciones=this.clientesService.getTransacciones();
+    transacciones = this.clientesService.getTransacciones();
     return transacciones;
   }
 
-  public casteoTransaccion(){
+  public casteoTransaccion() {
 
-    this.clientesService.getTransacciones2().subscribe((catsSnapshot) => {
+    this.clientesService.getusuarios().subscribe(async (catsSnapshot) => {
+
+
+
+    /*  catsSnapshot.forEach(async doc => {
+        console.log("Parent Document ID: ", doc.payload.doc.id);
+        let subCollectionDocs = await catsSnapshot(doc.payload.doc.id).collection("transaccion").get()
+        subCollectionDocs.forEach(subCollectionDoc => {
+          subCollectionDoc.forEach(doc => {
+            console.log("Sub Document ID: ", doc.id);
+          })
+   });*/
+
+      console.log(catsSnapshot)
+      for (let i = 0; i < catsSnapshot.length; i++) {
+        console.log("ENTRE AL FOR " + i)
+        console.log(catsSnapshot[i].payload.doc)
+
+      //  let subCollectionDocs = await catsSnapshot[i].payload.doc.collection("transaccion").get()
+      
+        this.array2.push(catsSnapshot[i].payload.doc.data())
+      }
+      console.log("ENTRE A CASTEO y el array 2 es "+this.array2[0])
       this.transacciones = [];
       catsSnapshot.forEach((TransData: any) => {
+        // console.log("ENTRE A FOR")
         this.transacciones.push({
-          id:TransData.payload.doc.id,
+          id: TransData.payload.doc.id,
           estado: TransData.payload.doc.Estado,
           fecha: TransData.payload.doc.Fecha,
           Gifcards: TransData.payload.doc.Gifcards,
           tarjeta: TransData.payload.doc.Tarjeta,
           total: TransData.payload.doc.Total
-  
+
         });
       })
     });
-  
+    //console.log("EL ARRAY ES " + this.transacciones[0]);
     return this.transacciones;
   }
 }
