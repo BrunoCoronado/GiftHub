2218dabe938616b4cd71675af9ed69d351ad7531
Ronald Romero <3606169920101@ingenieria.usac.edu.gt>
2020-10-13 19:35:48 -0600

    Falló la prueba de obtencion de transacciones

--- src/app/web.service.ts
+++ src/app/web.service.ts
@@ -1,38 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFirestore } from '@angular/fire/firestore';
-import { map,first } from 'rxjs/operators';
+
 @Injectable({
   providedIn: 'root'
 })
 export class WebService {
 
-  constructor(private afAuth?: AngularFireAuth, private firestore?: AngularFirestore) { 
+  constructor(private firestore: AngularFirestore) { 
     
   }
 
   getUsuarios(){
     console.log(this.firestore.collection('usuario'))
-  }
-
-  isLoggedIn() {
-    return this.afAuth.authState.pipe(first()).toPromise();
-  }
-
-  getCurrentUser(){
-    const user = this.isLoggedIn()
-    if (user) {
-      // do something
-      console.log('está dentro!');
-      return user;
-    } else {
-      // do something else
-      console.log("no hay nadie dentro!");
-      return undefined;
-    }
-  }
-  getUserTransactions(user:string){
-    return this.firestore.collection('usuario').get();
   }
 }
