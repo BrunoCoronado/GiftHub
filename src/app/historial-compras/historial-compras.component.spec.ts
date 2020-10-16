b98afcdcf0a423892266a0b4b64d59f39a51a584
Ronald Romero <3606169920101@ingenieria.usac.edu.gt>
2020-10-16 14:51:59 -0600

    Se eliminó una prueba que fallaba

--- src/app/historial-compras/historial-compras.component.spec.ts
+++ src/app/historial-compras/historial-compras.component.spec.ts
@@ -18,8 +18,4 @@ describe('HistorialComprasComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
