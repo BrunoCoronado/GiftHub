e2a6b4be5f4c3ada788ddb0892de2e5459ea7508
Ronald Romero <3606169920101@ingenieria.usac.edu.gt>
2020-10-22 15:09:21 -0600

    Primera prueba de CI

--- karma.conf.js
+++ karma.conf.js
@@ -7,8 +7,7 @@ module.exports = function (config) {
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
-      require('karma-chrome-launcher'),  /*comment out this line to disable the karma-chrome-launcher*/
-      //require('karma-phantomjs-launcher'),  /* add this line to disable the karma-phantomjs-launcher*/
+      require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
@@ -17,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/gifthub'),
+      dir: require('path').join(__dirname, '../coverage'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
@@ -26,20 +25,15 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ["Chrome", "ChromeHeadless"],
+    //browsers: ["Chrome", "ChromeHeadless"],
+    browsers: ['ChromeHeadlessNoSandbox'],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
     singleRun: false,
     restartOnFileChange: true,
-    customLaunchers: {
-      ChromeHeadless: {
-        base: "Chrome",
-        flags: [
-          "--headless",
-          "--disable-gpu",
-          "--no-sandbox",
-          "--remote-debugging-port=9222"
-        ],
-        
-      },
-    }
   });
 };
