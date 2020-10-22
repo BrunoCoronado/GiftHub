c2a46d00afc6450f689d2868b3e46eeadc30b6c9
Ronald Romero <3606169920101@ingenieria.usac.edu.gt>
2020-10-22 09:00:58 -0600

    Se agregó la configuración de ChromeHeadless

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -8,9 +8,9 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display Gifthub', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('gifthub app is running!');
+    expect(page.getTitleText()).toEqual('GiftHub');
   });
 
   afterEach(async () => {
