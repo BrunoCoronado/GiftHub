dd5c1338f584acd13f6b2c9f4c15cada077d54ec
Alan <alanmorataya19@gmail.com>
2020-10-29 02:36:08 -0600

    Se mostraron las transacciones en el front
    Falta corregir el formato de fecha mostrado y mostrar Giftcards

--- src/app/detalle-transacciones/detalle-transacciones.component.ts
+++ src/app/detalle-transacciones/detalle-transacciones.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { TransaccionesserviceService } from './transaccionesservice.service'
 import { MatTableDataSource } from '@angular/material/table';
+const ELEMENT_DATA: Transferencia[] = [];
 @Component({
   selector: 'app-detalle-transacciones',
   templateUrl: './detalle-transacciones.component.html',
   styleUrls: ['./detalle-transacciones.component.css']
 })
+
 export class DetalleTransaccionesComponent implements OnInit {
   // clientesService: TransaccionesserviceService;
   constructor(private servicio: TransaccionesserviceService) {
@@ -13,13 +15,15 @@ export class DetalleTransaccionesComponent implements OnInit {
   }
   public transacciones = [];
   public array2 = [];
-  dataSource = new MatTableDataSource<Element>(this.array2);
+ 
+data=Object.assign(ELEMENT_DATA)
+  dataSource = new MatTableDataSource<Element>(this.data);
   displayedColumns = [
     'select',
     'id',
     'Estado',
     'Fecha',
-    'Gifcards',
+    'Giftcards',
     'Tarjeta',
     'Total',
     'Nombre'
@@ -30,7 +34,8 @@ export class DetalleTransaccionesComponent implements OnInit {
     // this.transacciones = this.clientesService.gettransaccionesdetalladas();
     console.log(this.transacciones.length)
     this.Trasaccionescasteadas()
-    //console.log(this.transacciones)
+   
+
   }
 
   public getTransaccionestodos() {
@@ -42,17 +47,22 @@ export class DetalleTransaccionesComponent implements OnInit {
 
     this.servicio.getTransaccionesbd().subscribe(async (catsSnapshot) => {
 
+
+
       console.log(catsSnapshot)
       for (let i = 0; i < catsSnapshot.length; i++) {
         console.log("ENTRE AL FOR " + i)
         console.log(catsSnapshot[i].payload.doc)
-
+        
 
         this.array2.push(catsSnapshot[i].payload.doc.data())
+        this.array2[i].uid=i+1;
 
       }
-      console.log("EL NOMBRE DE LA PRIMER TRANSACCION FUE "+this.array2[0].nombre_tarjeta)
-
+      console.log("EL NOMBRE DE LA PRIMER TRANSACCION FUE "+this.array2[0].uid)
+      this.data=this.array2
+      this.dataSource = new MatTableDataSource<Element>(this.data);
+      console.log("LA DATA ES "+this.data[0].Giftcards[0])
       /*   console.log("ENTRE A CASTEO y el array 2 es " + this.array2[0])
          this.transacciones = [];
          catsSnapshot.forEach((TransData: any) => {
@@ -71,4 +81,14 @@ export class DetalleTransaccionesComponent implements OnInit {
     });
     return this.transacciones;
   }
+
+
+  
 }
+
+export class Transferencia {
+  constructor(public uid: string, public Estado: string, public Fecha: Date,public Gitfcards:[],public Tarjeta:string,public Total:number,public Nombre:string) 
+  {
+
+  }
+  }
